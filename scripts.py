2561dc0882769314a97c29321eac1b2b8504f036
AleksandrBara <alexbar130379@gmail.com>
2022-11-12 15:21:00 +0300

    Create scripts.

---
+++ scripts.py
@@ -0,0 +1,62 @@
+import random
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
+
+
+def find_schoolkid(schoolkid_name):
+    try:
+        return Schoolkid.objects.get(full_name=schoolkid_name)
+    except Schoolkid.DoesNotExist:
+        print('Студента {} не существует'.format(schoolkid_name))
+        print('Формат ввода: Фамилия-Имя-Отчество!')
+
+
+def fix_marks(schoolkid):
+    badmarks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    for badmark in badmarks:
+        badmark.points = "5"
+        badmark.save()
+
+
+def delete_chastisement(schoolkid):
+    chastisement = Chastisement.objects.filter(schoolkid=schoolkid[0])
+    chastisement.delete()
+
+
+def create_commendation(subject, schoolkid):
+    year_of_study = 6
+    group_letter = "А"
+    commendations = [
+        'Неожиданно!'
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!',
+    ]
+    commendation = random.choice(commendations)
+    lessons = Lesson.objects.filter(
+        year_of_study__in=[year_of_study],
+        group_letter=group_letter,
+        subject__title=subject,
+        )
+    lesson = random.choice(lessons)
+    Commendation.objects.create(
+        schoolkid=schoolkid,
+        subject=lesson.subject,
+        teacher=lesson.teacher,
+        created=lesson.date,
+        text=commendation,
+        )
