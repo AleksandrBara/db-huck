5de551f1e8970534357ec376945cbda9d37dd1cb
AleksandrBara <alexbar130379@gmail.com>
2022-11-14 10:52:08 +0300

    Ввел глобальную переменную.

--- scripts.py
+++ scripts.py
@@ -2,6 +2,28 @@ import random
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 
 
+COMMENDATIONS = [
+        'Неожиданно!'
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!',
+    ]
+
 def find_schoolkid(schoolkid_name):
     try:
         return Schoolkid.objects.get(full_name=schoolkid_name)
@@ -25,28 +47,7 @@ def delete_chastisement(schoolkid):
 def create_commendation(subject, schoolkid):
     year_of_study = 6
     group_letter = "А"
-    commendations = [
-        'Неожиданно!'
-        'Талантливо!',
-        'Ты сегодня прыгнул выше головы!',
-        'Я поражен!',
-        'Уже существенно лучше!',
-        'Потрясающе!',
-        'Замечательно!',
-        'Прекрасное начало!',
-        'Так держать!',
-        'Ты на верном пути!',
-        'Здорово!',
-        'Это как раз то, что нужно!',
-        'Я тобой горжусь!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Мы с тобой не зря поработали!',
-        'Я вижу, как ты стараешься!',
-        'Ты растешь над собой!',
-        'Ты многое сделал, я это вижу!',
-        'Теперь у тебя точно все получится!',
-    ]
-    commendation = random.choice(commendations)
+    commendation = random.choice(COMMENDATIONS)
     lessons = Lesson.objects.filter(
         year_of_study__in=[year_of_study],
         group_letter=group_letter,
